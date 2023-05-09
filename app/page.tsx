e35365b8cb3bb6a73e5ea17c3c060585eaf3b5e2
YYODHH <71649114+YYODHH@users.noreply.github.com>
2023-05-09 23:01:21 +0200

    visit counter

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-export default function Home() {
-  console.log("Hello, World!");
+import kv from "@vercel/kv";
+
+export default async function Home() {
+  await kv.incr("counter");
+  const counter = (await kv.get("counter")) as number;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Hello, World!</h1>
+      <h1 className="text-6xl font-bold">{counter}</h1>
     </main>
   );
 }
